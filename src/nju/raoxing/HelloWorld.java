b65ed70651ee36f36dbbad8018700aa5fc9a2ff1
09star <raoxing.nju@gmail.com>
2013-04-01 16:47:00 +0800

    init the project

---
+++ src/nju/raoxing/HelloWorld.java
@@ -0,0 +1,14 @@
+package nju.raoxing;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: beyondsoft_ets04
+ * Date: 13-4-1
+ * Time: 下午3:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void  main(String [] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/nju/raoxing/StarMonitor.java
@@ -0,0 +1,14 @@
+package nju.raoxing;
+/**
+ * Created with IntelliJ IDEA.
+ * User: beyondsoft_ets04
+ * Date: 13-4-1
+ * Time: 下午4:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class StarMonitor {
+    public static void  main(String [] args){
+        System.out.println("Hello This StarMonitor!");
+    }
+
+}
