af9696109e57252dd08ddcd0de3f7fa7d4628350
09star <raoxing.nju@gmail.com>
2013-04-02 14:13:40 +0800

    init the project

--- src/nju/raoxing/StarMonitor.java
+++ src/nju/raoxing/StarMonitor.java
@@ -6,9 +6,26 @@ package nju.raoxing;
  * Time: 下午4:42
  * To change this template use File | Settings | File Templates.
  */
-public class StarMonitor {
-    public static void  main(String [] args){
-        System.out.println("Hello This StarMonitor!");
-    }
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.Tlhelp32;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinNT;
+import com.sun.jna.win32.W32APIOptions;
+import com.sun.jna.Native;
 
+public class StarMonitor {
+    public static void main(String[] args) {
+        Kernel32 kernel32 = (Kernel32) Native.loadLibrary(Kernel32.class, W32APIOptions.UNICODE_OPTIONS);
+        Tlhelp32.PROCESSENTRY32.ByReference processEntry = new Tlhelp32.PROCESSENTRY32.ByReference();
+
+        WinNT.HANDLE snapshot = kernel32.CreateToolhelp32Snapshot(Tlhelp32.TH32CS_SNAPPROCESS, new WinDef.DWORD(0));
+        try  {
+            while (kernel32.Process32Next(snapshot, processEntry)) {
+                System.out.println(processEntry.th32ProcessID + "\t" + Native.toString(processEntry.szExeFile));
+            }
+        }
+        finally {
+            kernel32.CloseHandle(snapshot);
+        }
+    }
 }
